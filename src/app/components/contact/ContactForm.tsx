5f3c3423fdfe2bdb3a35751d46cbca86aa2c25c2
fpmeeloop <kootblad@live.nl>
2024-03-15 08:37:02 +0100

    minor changes

---
+++ src/app/components/contact/ContactForm.tsx
@@ -0,0 +1,44 @@
+import React from 'react'
+
+export const ContactForm = () => {
+  return (
+    <form className="w-full max-w-md">
+        <div className="mb-4">
+            <label htmlFor="name" className="mb-2 text-gray-500">Name</label>
+            <input
+                type="text"
+                id="name"
+                placeholder="John Doe"
+                required
+                className="p-2 bg-transparent border-2 border-gray-500 rounded focus:outline-none focus:border-blue-500 w-full"
+            />
+        </div>
+        <div className="mb-4">
+            <label htmlFor="email" className="mb-2 text-gray-500">Email</label>
+            <input
+                type="email"
+                id="email"
+                placeholder="your@email.com"
+                required
+                className="p-2 bg-transparent border-2 border-gray-500 rounded focus:outline-none focus:border-blue-500 w-full"
+            />
+        </div>
+        <div className="mb-4">
+            <label htmlFor="message" className="mb-2 text-gray-500">Message</label>
+            <textarea
+                id="message"
+                required
+                placeholder="How can we assist you?"
+                rows={4}
+                className="p-2 bg-transparent border-2 border-gray-500 rounded focus:outline-none focus:border-blue-500 w-full"
+            ></textarea>
+        </div>
+        <button
+            type="submit"
+            className="py-2 px-4 bg-blue-500 hover:bg-blue-700 text-white font-bold rounded transition-colors duration-300"
+        >
+            Send message
+        </button>
+    </form>
+  )
+}
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -5,6 +5,7 @@ import {MotionHeader1} from '../components/frames/MotionHeader1';
 import { MotionParagraph } from '../components/frames/MotionParagraph';
 import { MotionDiv } from '../components/frames/MotionDiv';
 import { ContactInfo, SocialIcons } from '../components/Icons';
+import { ContactForm } from '../components/contact/ContactForm';
 
 
 export const metadata: Metadata = {
@@ -72,44 +73,7 @@ export default function Contact() {
                         transition={{ duration: 0.5, delay: 0.6 }}
                         className="flex flex-col items-center"
                     >
-                        <form className="w-full max-w-md">
-                            <div className="mb-4">
-                                <label htmlFor="name" className="mb-2 text-gray-500">Name</label>
-                                <input
-                                    type="text"
-                                    id="name"
-                                    placeholder="John Doe"
-                                    required
-                                    className="p-2 bg-transparent border-2 border-gray-500 rounded focus:outline-none focus:border-blue-500 w-full"
-                                />
-                            </div>
-                            <div className="mb-4">
-                                <label htmlFor="email" className="mb-2 text-gray-500">Email</label>
-                                <input
-                                    type="email"
-                                    id="email"
-                                    placeholder="your@email.com"
-                                    required
-                                    className="p-2 bg-transparent border-2 border-gray-500 rounded focus:outline-none focus:border-blue-500 w-full"
-                                />
-                            </div>
-                            <div className="mb-4">
-                                <label htmlFor="message" className="mb-2 text-gray-500">Message</label>
-                                <textarea
-                                    id="message"
-                                    required
-                                    placeholder="How can we assist you?"
-                                    rows={4}
-                                    className="p-2 bg-transparent border-2 border-gray-500 rounded focus:outline-none focus:border-blue-500 w-full"
-                                ></textarea>
-                            </div>
-                            <button
-                                type="submit"
-                                className="py-2 px-4 bg-blue-500 hover:bg-blue-700 text-white font-bold rounded transition-colors duration-300"
-                            >
-                                Send message
-                            </button>
-                        </form>
+                        <ContactForm />
                     </MotionDiv>
                 </div>
             </div>
