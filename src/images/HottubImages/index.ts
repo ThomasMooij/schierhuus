88c55c183b83780aad4f17435d17567ae3d46d69
Thomas Mooij <kootblad@live.nl>
2024-09-10 12:37:02 +0200

    live live v1

--- src/images/HottubImages/index.ts
+++ src/images/HottubImages/index.ts
@@ -1,7 +1,6 @@
-import { StaticImageData } from 'next/image';
-import image1 from './hottub1.jpg';
-import image2 from './hottub2.jpg';
-
-const HottubImages: StaticImageData[]
-= [image1, image2];
-export default HottubImages;
+const HottubImages: string[] = [
+    "https://storage.googleapis.com/schierhuus_bucket/hottub/hottub1.jpg",
+    "https://storage.googleapis.com/schierhuus_bucket/hottub/hottub2.jpg",
+  ];
+  
+  export default HottubImages;
